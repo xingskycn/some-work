1dc857795590aca9a8c3608a0af158940be46622
Sebastian Pretscher <s.pretscher@gmail.com>
2011-10-06 23:57:20 +0200

    Initial Project commit

---
+++ JBump/JBump/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  JBump
+//
+//  Created by Sebastian Pretscher on 06.10.11.
+//  Copyright TU München 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
