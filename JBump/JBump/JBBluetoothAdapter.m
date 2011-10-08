153cceb310d465c6e20bb2ec0a21c481208e1ef8
Sebastian Pretscher <s.pretscher@gmail.com>
2011-10-08 21:18:48 +0200

    ...2

---
+++ JBump/JBump/JBBluetoothAdapter.m
@@ -0,0 +1,13 @@
+//
+//  JBBluetoothAdapter.m
+//  JBump
+//
+//  Created by Sebastian Pretscher on 08.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "JBBluetoothAdapter.h"
+
+@implementation JBBluetoothAdapter
+
+@end
