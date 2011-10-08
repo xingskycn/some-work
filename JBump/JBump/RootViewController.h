f27b0cbb872f3a938f973b95059c4c4ec7cae701
Sebastian Pretscher <s.pretscher@gmail.com>
2011-10-08 14:21:50 +0200

    commiting new initial project

---
+++ JBump/JBump/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  JBump
+//
+//  Created by Sebastian Pretscher on 08.10.11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
