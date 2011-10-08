e8a6324fe2b80e82c1e329feae12446d4c7fd8ac
Nils Ziehn <nziehn@nziehn-book.local>
2011-10-08 18:26:16 +0200

    added the map creator project

---
+++ trunk/MapCreator/MapCreator/MapLayer.h
@@ -0,0 +1,26 @@
+//
+//  HelloWorldLayer.h
+//  MapCreator
+//
+//  Created by Nils Ziehn on 10/8/11.
+//  Copyright ziehn.org 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+#import "Box2D.h"
+#import "GLES-Render.h"
+
+// HelloWorldLayer
+@interface MapLayer : CCLayer
+{
+	b2World* world;
+	GLESDebugDraw *m_debugDraw;
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+
+@end
---
+++ trunk/MapCreator/MapCreator/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  MapCreator
+//
+//  Created by Nils Ziehn on 10/8/11.
+//  Copyright ziehn.org 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
