d01dde6d7697980ec21bedb83d82c12582722c63
Sebastian Pretscher <s.pretscher@gmail.com>
2011-10-09 23:57:33 +0200

    added gameviewcontroller

---
+++ JBump/JBump/JBGameViewController.mm
@@ -0,0 +1,74 @@
+//
+//  JBGameViewController.m
+//  JBump
+//
+//  Created by Sebastian Pretscher on 09.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "JBGameViewController.h"
+#import "JBGameLayer.h";
+
+@implementation JBGameViewController
+
+@synthesize gameLayer;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    CCScene* scene = [JBGameLayer scene];
+    self.gameLayer = (JBGameLayer *)[scene getChildByTag:0];
+    
+    [[CCDirector sharedDirector] replaceScene:scene];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)dealloc {
+    
+    self.gameLayer=nil;
+    
+    [super dealloc];
+}
+
+@end
